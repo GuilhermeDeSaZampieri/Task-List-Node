1bb584a4e90ed18a068f78f665e8bcf1d3457121
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-04-10 17:27:26 -0300

    add fetch function

--- Front-end/script.js
+++ Front-end/script.js
@@ -1,66 +1,83 @@
-const finishButtons = document.querySelectorAll(".tasks ul li button");
+const fetchTasks = async () => {
+  const res = await fetch("http://localhost:3333/tasks/get");
+  return res.json();
+};
 
-finishButtons.forEach((button) => {
-  button.addEventListener("click", finishTask);
-});
+const createElement = (tag, innerText = "") => {
+  const element = document.createElement(tag);
+  element.innerText = innerText;
+  return element;
+};
 
-function finishTask(event) {
-  const li = event.target.parentElement.parentElement;
-  li.classList.toggle("done");
+const createRow = (task) => {
+  const { id, title, created_at, status } = task;
+  const tr = createElement("tr");
+  const tdTitle = createElement("td", "Titulo da task");
+};
 
-  if (li.classList.contains("done")) {
-    event.target.innerText = "Desfazer";
-  } else {
-    event.target.innerText = "Finalizar";
-  }
-}
+// const finishButtons = document.querySelectorAll(".tasks ul li button");
 
-function removeTask(event) {
-  const li = event.target.parentElement.parentElement;
-  const ul = li.parentElement;
-  ul.removeChild(li);
-}
+// finishButtons.forEach((button) => {
+//   button.addEventListener("click", finishTask);
+// });
 
-const form = document.querySelector("form");
+// function finishTask(event) {
+//   const li = event.target.parentElement.parentElement;
+//   li.classList.toggle("done");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault(); //tira o reload da pagina
+//   if (li.classList.contains("done")) {
+//     event.target.innerText = "Desfazer";
+//   } else {
+//     event.target.innerText = "Finalizar";
+//   }
+// }
 
-  const input = document.querySelector("input");
-  const taskText = input.value;
+// function removeTask(event) {
+//   const li = event.target.parentElement.parentElement;
+//   const ul = li.parentElement;
+//   ul.removeChild(li);
+// }
 
-  if (taskText === "") {
-    alert("Escreva alguma coisa");
-    return;
-  }
+// const form = document.querySelector("form");
 
-  const existingTasks = document.querySelectorAll(".tasks ul li span");
-  existingTasks.forEach((span) => {
-    if (span.innerText.toLowerCase() === taskText.toLowerCase()) {
-      alert("Já foi informada uma atividade com esse título");
-      taskText = "";
-    }
-  });
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault(); //tira o reload da pagina
 
-  const buttonFinish = document.createElement("button");
-  buttonFinish.classList.add("btn");
-  buttonFinish.innerText = "Finalizar";
-  buttonFinish.addEventListener("click", finishTask);
+//   const input = document.querySelector("input");
+//   const taskText = input.value;
 
-  const buttonDelete = document.createElement("button");
-  buttonDelete.classList.add("btn");
-  buttonDelete.addEventListener("click", removeTask);
-  buttonDelete.innerText = "Excluir";
+//   if (taskText === "") {
+//     alert("Escreva alguma coisa");
+//     return;
+//   }
 
-  const li = document.createElement("li");
-  li.innerHTML = "<span>" + taskText + "</span>";
+//   const existingTasks = document.querySelectorAll(".tasks ul li span");
+//   existingTasks.forEach((span) => {
+//     if (span.innerText.toLowerCase() === taskText.toLowerCase()) {
+//       alert("Já foi informada uma atividade com esse título");
+//       taskText = "";
+//     }
+//   });
 
-  const buttonBox = document.createElement("div");
-  buttonBox.classList.add("btn-box");
-  buttonBox.appendChild(buttonFinish);
-  buttonBox.appendChild(buttonDelete);
-  li.appendChild(buttonBox);
+//   const buttonFinish = document.createElement("button");
+//   buttonFinish.classList.add("btn");
+//   buttonFinish.innerText = "Finalizar";
+//   buttonFinish.addEventListener("click", finishTask);
 
-  const ul = document.querySelector(".tasks ul");
-  ul.prepend(li); //adiciona arquivos ao mais recente
-});
+//   const buttonDelete = document.createElement("button");
+//   buttonDelete.classList.add("btn");
+//   buttonDelete.addEventListener("click", removeTask);
+//   buttonDelete.innerText = "Excluir";
+
+//   const li = document.createElement("li");
+//   li.innerHTML = "<span>" + taskText + "</span>";
+
+//   const buttonBox = document.createElement("div");
+//   buttonBox.classList.add("btn-box");
+//   buttonBox.appendChild(buttonFinish);
+//   buttonBox.appendChild(buttonDelete);
+//   li.appendChild(buttonBox);
+
+//   const ul = document.querySelector(".tasks ul");
+//   ul.prepend(li); //adiciona arquivos ao mais recente
+// });

c726530df110d1aba4d24819f95454ea80c98c95
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-05-25 09:20:46 -0300

    Criandoo front end

--- Front-end/script.js
+++ Front-end/script.js
@@ -26,7 +26,7 @@ const addtask = async (event) => {
 
 const deleteTask = async (id) => {
   await fetch(`http://localhost:3333/tasks/${id}`, {
-    method: "delete",
+    method: "delete", 
   });
 
   loadTasks();
